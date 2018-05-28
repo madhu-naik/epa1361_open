dd124d5425c2224ed5c1a2794919752a38deafe2
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2018-05-28 18:53:33 +0200

    autopep8

--- final assignment/dike_model_optimization.py
+++ final assignment/dike_model_optimization.py
@@ -1,14 +1,14 @@
 from __future__ import (unicode_literals, print_function, absolute_import,
-                                        division)
+                        division)
 
 
-from ema_workbench import (Model, MultiprocessingEvaluator, 
+from ema_workbench import (Model, MultiprocessingEvaluator,
                            ScalarOutcome, IntegerParameter, optimize, Scenario)
 from ema_workbench.em_framework.optimization import EpsilonProgress, HyperVolume
 from ema_workbench.util import ema_logging
 
 from problem_formulation import get_model_for_problem_formulation
-import matplotlib.pyplot as plt    
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
@@ -16,42 +16,41 @@ if __name__ == '__main__':
     ema_logging.log_to_stderr(ema_logging.INFO)
 
     dike_model = get_model_for_problem_formulation(2)
-    
-    reference_values = {'Bmax': 175, 'Brate': 1.5, 'pfail': 0.5, 
+
+    reference_values = {'Bmax': 175, 'Brate': 1.5, 'pfail': 0.5,
                         'discount rate': 3.5,
                         'ID flood wave shape': 4}
     scen1 = {}
-    
-    for key in dike_model.uncertainties:
-            name_split = key.name.split('_')
 
-            if len(name_split) == 1:
-                 scen1.update({key.name: reference_values[key.name]})
-                                
-            else:                 
-                 scen1.update({key.name: reference_values[name_split[1]]})
-        
+    for key in dike_model.uncertainties:
+        name_split = key.name.split('_')
+
+        if len(name_split) == 1:
+            scen1.update({key.name: reference_values[key.name]})
+
+        else:
+            scen1.update({key.name: reference_values[name_split[1]]})
+
     ref_scenario = Scenario('reference', **scen1)
 
     convergence_metrics = [EpsilonProgress()]
-    
-    espilon = [1e3]*len(dike_model.outcomes)
+
+    espilon = [1e3] * len(dike_model.outcomes)
 
     nfe = 200
 
 # OPTIMIZATION:
 #    results, convergence = optimize(dike_model, nfe=nfe, searchover='levers',
-#                                    epsilons=espilon, convergence = convergence_metrics, 
+#                                    epsilons=espilon, convergence = convergence_metrics,
 #                                    reference = ref_scenario)
-#    
+#
     with MultiprocessingEvaluator(dike_model) as evaluator:
-        results, convergence = evaluator.optimize(nfe = nfe,
-                                  searchover = 'levers', 
-                                  epsilons = espilon,
-                                  convergence = convergence_metrics, 
-                                  reference = ref_scenario
-                                  )
-
+        results, convergence = evaluator.optimize(nfe=nfe,
+                                                  searchover='levers',
+                                                  epsilons=espilon,
+                                                  convergence=convergence_metrics,
+                                                  reference=ref_scenario
+                                                  )
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=True)
     fig, ax1 = plt.subplots(ncols=1)
@@ -60,6 +59,4 @@ if __name__ == '__main__':
     ax1.set_ylabel('$\epsilon$ progress')
 #    ax2.plot(convergence.hypervolume)
 #    ax2.set_ylabel('hypervolume')
-    sns.despine()    
-    
-    
+    sns.despine()
